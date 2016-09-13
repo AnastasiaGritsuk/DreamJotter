e25aecefc50b17be87d3f9499b4d4e8972df5830
AnastasiaGritsuk <natikana_@mail.ru>
2016-09-13 16:04:23 +0300

    1st jasmine test 1

--- spec/jotterSpec.js
+++ spec/jotterSpec.js
@@ -4,7 +4,7 @@ var base_url = "http://localhost:3000/";
 
 describe("Jotter Server", function() {
     describe("GET /", function() {
-        it("returns status code 200", function(done) {
+        it("returns status code 200 ", function(done) {
             request.get(base_url, function(error, response, body) {
                 expect(response.statusCode).toBe(200);
                 done();
