56370a93c939adca4c006d0337a5316bf489e238
AnastasiaGritsuk <natikana_@mail.ru>
2016-09-13 15:18:46 +0300

    1st jasmine test

--- routes/app.js
+++ routes/app.js
@@ -9,6 +9,67 @@ router.get('/', function(req, res, next) {
     res.render('index');
 });
 
+router.post('/auth', function(req, res, next) {
+    var token = Guid.create();
+    var creds = basicAuthParser(req.headers.authorization);
+    var username = creds.username;
+    var password = creds.password;
+
+    User.count(function (err, count) {
+        if (!err && count === 0) {
+            populateDB();
+        }else {
+            User.findOne({username: username}, function (err, doc) {
+                if(err) {
+                    console.log('unhandled error');
+                    throw  err;
+                }
+                if(doc) {
+                    if(doc.password === password) {
+                        doc.securityToken = token;
+                        doc.save();
+
+                        return res.status(200).json({
+                            data: token
+                        });
+                    }
+                }
+                return res.status(401).send('Unauthorized');
+            });
+        }
+    });
+
+    function populateDB() {
+        console.log('population starts');
+
+        var newuser = {
+            username:  username,
+            password: password,
+            securityToken: token
+        };
+        var user = new User(newuser);
+        user.save();
+
+        return res.status(200).json({
+            data: token
+        });
+    }
+});
+
+router.delete('/auth', function(req, res, next) {
+    var token = req.headers.authorization;
+
+    User.findOne({securityToken: token}, function (err, doc) {
+        if(err) throw  err;
+        if(doc) {
+            doc.securityToken = null;
+            return res.status(200).send('Logout successful');
+        }
+        return res.status(401).send('Unauthorized');
+    });
+});
+
+
 router.get('/note/:key', function(req, res, next) {
     var token = req.headers.authorization;
     var key = req.params.key;
@@ -21,7 +82,6 @@ router.get('/note/:key', function(req, res, next) {
 
             UserNote.find({user: user, name: key}, function (err, doc) {
                 if(err) throw  err;
-                console.log('DOC ' + doc.length);
                 if(doc.length !== 0) {
                     doc.forEach(function (note) {
                         result.push(note);
@@ -60,64 +120,5 @@ router.post('/note', function(req, res, next) {
     });
 });
 
-router.post('/auth', function(req, res, next) {
-    var token = Guid.create();
-    var creds = basicAuthParser(req.headers.authorization);
-    var username = creds.username;
-    var password = creds.password;
-
-    User.count(function (err, count) {
-        if (!err && count === 0) {
-            populateDB();
-        }else {
-            User.findOne({username: username}, function (err, doc) {
-                if(err) {
-                    console.log('unhandled error');
-                    throw  err;
-                }
-                if(doc) {
-                    if(doc.password === password) {
-                        doc.securityToken = token;
-                        doc.save();
-
-                        return res.status(200).json({
-                            data: token
-                        });
-                    }
-                }
-                return res.status(401).send('Unauthorized');
-            });
-        }
-    });
-    
-    function populateDB() {
-        console.log('population starts');
-
-        var newuser = {
-            username:  username,
-            password: password,
-            securityToken: token
-        };
-        var user = new User(newuser);
-        user.save();
-
-        return res.status(200).json({
-            data: token
-        });
-    }
-});
-
-router.delete('/auth', function(req, res, next) {
-    var token = req.headers.authorization;
-
-    User.findOne({securityToken: token}, function (err, doc) {
-        if(err) throw  err;
-        if(doc) {
-            doc.securityToken = null;
-            return res.status(200).send('Logout successful');
-        }
-        return res.status(401).send('Unauthorized');
-    });
-});
 
 module.exports = router;
