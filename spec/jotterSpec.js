091d7ba95d866c0366dfa3d2553ddfaef1afc289
AnastasiaGritsuk <natikana_@mail.ru>
2016-09-27 13:39:18 +0300

    "POST /auth" full set

--- spec/jotterSpec.js
+++ spec/jotterSpec.js
@@ -6,7 +6,7 @@ describe("Jotter Server", function() {
 
     describe("POST /auth", function() {
 
-        it("user login with valid username and password ", function(done) {
+        it("user login with valid username and valid password ", function(done) {
             var creds = new Buffer('admin' + ':' + '1234').toString('base64');
             var headers = {
                 'Content-Type': 'application/json',
@@ -24,6 +24,38 @@ describe("Jotter Server", function() {
         });
         it("user login with valid username and invalid password ", function(done) {
             var creds = new Buffer('admin' + ':' + 'zzz').toString('base64');
+            var headers = {
+                'Content-Type': 'application/json',
+                'Authorization' : 'Basic ' + creds
+            };
+
+            request.post({
+                headers: headers,
+                url: base_url + '/auth',
+                body: ''
+            }, function(error, response, body) {
+                expect(response.statusCode).toBe(401);
+                done();
+            });
+        });
+        it("user login with invalid username and invalid password ", function(done) {
+            var creds = new Buffer('zzz' + ':' + 'zzz').toString('base64');
+            var headers = {
+                'Content-Type': 'application/json',
+                'Authorization' : 'Basic ' + creds
+            };
+
+            request.post({
+                headers: headers,
+                url: base_url + '/auth',
+                body: ''
+            }, function(error, response, body) {
+                expect(response.statusCode).toBe(401);
+                done();
+            });
+        });
+        it("user login with invalid username and valid password ", function(done) {
+            var creds = new Buffer('zzz' + ':' + '1234').toString('base64');
             var headers = {
                 'Content-Type': 'application/json',
                 'Authorization' : 'Basic ' + creds
