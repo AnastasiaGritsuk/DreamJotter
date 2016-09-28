de8e55ca2e7aed25a8d95795c2da7d37495c5dc5
AnastasiaGritsuk <natikana_@mail.ru>
2016-09-28 12:48:39 +0300

    btoa in utils

---
+++ common/utils.js
@@ -0,0 +1,9 @@
+function btoa(str) {
+    if (Buffer.byteLength(str) !== str.length)
+        throw new Error('bad string!');
+    return Buffer(str, 'binary').toString('base64');
+}
+
+module.exports = {
+    btoa:btoa
+}
--- spec/jotterSpec.js
+++ spec/jotterSpec.js
@@ -1,12 +1,7 @@
 var request = require("request");
+var btoa = require("../common/utils").btoa;
 
 var base_url = "http://localhost:3000";
-
-function btoa(str) {
-    if (Buffer.byteLength(str) !== str.length)
-        throw new Error('bad string!');
-    return Buffer(str, 'binary').toString('base64');
-}
 
 function loginHeaders(username, pwd) {
     return {
