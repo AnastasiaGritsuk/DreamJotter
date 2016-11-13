62e9a52b8a1bbc7b6c086cf2a3bf16dd6b936a3e
AnastasiaGritsuk <natikana_@mail.ru>
2016-11-13 20:01:33 +0300

    404 error handling fix

--- server/app.js
+++ server/app.js
@@ -37,7 +37,7 @@ app.use('/admin', adminRoute);
 
 // error handlers
 
-app.use(function (req, res, next) {
+app.get('*', function (req, res, next) {
     var err = new Error('Not found');
     err.status = 404;
     next(err);
